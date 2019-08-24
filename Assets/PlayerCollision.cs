bae1742754711726b814ed3e4c0285b6e38bc13b
brendan giraudet <brendan.giraudet1@gmail.com>
2019-08-24 15:51:21 +0200

    First game : run game step 1-5

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMove movement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag.Equals("Obstacle"))
+        {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    /// <summary>
+    /// Reference to the Rigidbody component called "rb"
+    /// </summary>
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    /// <summary>
+    /// Mess with physics
+    /// </summary>
+    void FixedUpdate()
+    {
+        // Add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("q"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
